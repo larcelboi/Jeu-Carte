94b0bf52f52bc9cc29f880e717ef9dc33c6fa67f
larcelboi <2355315@cegepoutaouais.qc.ca>
2025-10-14 15:45:42 -0400

    creation 12 carte

--- script_jeu/script1.js
+++ script_jeu/script1.js
@@ -2,16 +2,23 @@ const btnCommencer = document.querySelector("#btnCom");
 const boxCarte = document.querySelector("#boxC");
 
 btnCommencer.addEventListener("click", (e) => {
-    // Création carte
-    const carte = document.createElement("div");
-    carte.classList.add("carte");
-    const sous_carte = document.createElement("div");
-    sous_carte.classList.add("sous_carte");
 
-    sous_carte.textContent = "?";
+    // fonction création 12 carte
+    function  CreationCarte(){
+        for (let i = 0; i < 12; i++) {
+            // Création carte
+            const carte = document.createElement("div");
+            carte.classList.add("carte");
+            const sous_carte = document.createElement("div");
+            sous_carte.classList.add("sous_carte");
 
-    // Ajouter sous carte dans carte
-    carte.append(sous_carte);
+            sous_carte.textContent = "?";
+            // Ajouter sous carte dans carte
+            carte.append(sous_carte);
 
-    boxCarte.append(carte)
+            boxCarte.append(carte);
+        }
+    }
+    CreationCarte();
+
 })
