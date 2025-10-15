b6203fa7f26a11f417ed65c49b5c42684e95a0a1
larcel <2355315@cegepoutaouais.qc.ca>
2025-10-15 12:31:23 -0400

    adasd

--- script_jeu/script1.js
+++ script_jeu/script1.js
@@ -14,6 +14,7 @@ btnCommencer.addEventListener("click", (e) => {
     }
     commencer_prtie = true
     boxCarte.style.display = "flex";
+
     function  CreationCarte(){
         for (let i = 0; i < 12; i++) {
             // Création carte
@@ -36,16 +37,31 @@ btnCommencer.addEventListener("click", (e) => {
 boxCarte.addEventListener("click", (e) => {
     const carte_choisie = e.target;
     const type_emoji = ["🍇","🍈","🍉","🍊","🍋","🍏"]
+    if (type_emoji.includes(carte_choisie.textContent)) {
+        alert("Veuillez sélectionner une autre carte")
+        return
+    }
     if (carte_choisie.id in dict_carte_emoji) {
         carte_choisie.textContent = dict_carte_emoji[carte_choisie.id];
-        Carte();
+        reaffichageCarte();
     }
     else{
         Carte();
     }
+    function reaffichageCarte() {
+        if (e.target.classList.contains("sous_carte")) {
+            console.log(carte_choisie);
+            carte_choisie.style.transition = "backgroundcolor 0.5 ease-in-out,color 0.5 ease-in-out ";
+            carte_choisie.style.backgroundColor = "white";
+            carte_choisie.style.color = "white";
 
+            setTimeout(() => {
+                carte_choisie.style.backgroundColor = "black";
+                carte_choisie.style.color = "white";
+            }, 100 * 8)
+        }
+    }
     function Carte() {
-
         if (e.target.classList.contains("sous_carte")) {
             console.log(carte_choisie);
             carte_choisie.style.transition = "backgroundcolor 0.5 ease-in-out,color 0.5 ease-in-out ";
@@ -59,6 +75,7 @@ boxCarte.addEventListener("click", (e) => {
             }, 100 * 8)
         }
     }
+
     function ChangerText(type_emoji){
         while (true){
             let index_dans_liste = Math.floor(Math.random() * type_emoji.length)
