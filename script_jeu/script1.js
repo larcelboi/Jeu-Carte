4d327746906f1063fa17ebb0d9b7d08ef1bbbc2d
larcel <2355315@cegepoutaouais.qc.ca>
2025-10-15 00:51:52 -0400

    creation 12 carte

--- script_jeu/script1.js
+++ script_jeu/script1.js
@@ -1,9 +1,16 @@
 const btnCommencer = document.querySelector("#btnCom");
 const boxCarte = document.querySelector("#boxC");
+const liste_chiffre = []
+let commencer_prtie = false
 
 btnCommencer.addEventListener("click", (e) => {
-
     // fonction création 12 carte
+    if (commencer_prtie) {
+        alert("la partie a déjà commencer")
+        return;
+    }
+    commencer_prtie = true
+    boxCarte.style.display = "flex";
     function  CreationCarte(){
         for (let i = 0; i < 12; i++) {
             // Création carte
@@ -12,13 +19,46 @@ btnCommencer.addEventListener("click", (e) => {
             const sous_carte = document.createElement("div");
             sous_carte.classList.add("sous_carte");
 
-            sous_carte.textContent = "?";
+            sous_carte.textContent = "❓";
             // Ajouter sous carte dans carte
             carte.append(sous_carte);
 
-            boxCarte.append(carte);
+            boxCarte.appendChild(carte);
         }
     }
     CreationCarte();
 
 })
+boxCarte.addEventListener("click", (e) => {
+    const carte_choisie = e.target;
+    const type_emoji = ["🍇","🍈","🍉","🍊","🍋","🍏"]
+
+    if (e.target.classList.contains("sous_carte")) {
+        console.log(carte_choisie);
+        carte_choisie.style.transition = "backgroundcolor 0.5 ease-in-out,color 0.5 ease-in-out ";
+        carte_choisie.style.backgroundColor = "white";
+        carte_choisie.style.color = "white";
+
+        setTimeout(()=>{
+            carte_choisie.style.backgroundColor = "black";
+            carte_choisie.style.color = "white";
+            ChangerText(type_emoji)
+        },100 * 8)
+
+        function ChangerText(type_emoji){
+            while (true){
+                let index_dans_liste = Math.floor(Math.random() * type_emoji.length)
+                const repetition_chiffre = liste_chiffre.filter( chiffre => chiffre === index_dans_liste).length
+                if (repetition_chiffre < 2 ) {
+                    liste_chiffre.push(index_dans_liste)
+                    carte_choisie.textContent = type_emoji[index_dans_liste];
+                    break
+                }
+            }
+        }
+    }
+
+
+
+})
+
