79d0f16839b4faeb2c13a69d9bc5bc6921f7902a
larcel <2355315@cegepoutaouais.qc.ca>
2025-10-16 08:00:04 -0400

    almost ddone

--- script_jeu/script1.js
+++ script_jeu/script1.js
@@ -4,6 +4,8 @@ const liste_chiffre = []
 const liste_carte_paire = []
 const dict_carte_emoji = {}
 let commencer_prtie = false
+let nombre_carte_valie = 1
+
 // try to use inner html with this sh
 
 btnCommencer.addEventListener("click", (e) => {
@@ -16,18 +18,22 @@ btnCommencer.addEventListener("click", (e) => {
     boxCarte.style.display = "flex";
 
     function  CreationCarte(){
+        // Création des 12 cartes
         for (let i = 0; i < 12; i++) {
             // Création carte
             const carte = document.createElement("div");
+            // Appliquer css carte
             carte.classList.add("carte");
             const sous_carte = document.createElement("div");
+            //Donner un ID au carte
             sous_carte.id = `carte-${i}`;
+            // Appliquer css sous_carte
             sous_carte.classList.add("sous_carte");
-
+            // Changer le text à ?
             sous_carte.textContent = "❓";
             // Ajouter sous carte dans carte
             carte.append(sous_carte);
-
+            //Ajouter tous les cartes dans la boite de carte
             boxCarte.appendChild(carte);
         }
     }
@@ -42,25 +48,29 @@ boxCarte.addEventListener("click", (e) => {
         alert("Veuillez sélectionner une autre carte")
         return
     }
-
+    // Redonner l'emoji à la carte qui a été retourner
     if (carte_choisie.id in dict_carte_emoji) {
-        carte_choisie.textContent = dict_carte_emoji[carte_choisie.id];
-        retransitionEmojiCarte();
+        // Prend l'emoji de l'id de la carte
+        const emoji_id = dict_carte_emoji[carte_choisie.id];
+        reTransitionEmojiCarte(emoji_id);
     }
     else{
         transitionEmojiCarte();
     }
 
-    function retransitionEmojiCarte() {
+    function reTransitionEmojiCarte(emoji_id) {
         if (e.target.classList.contains("sous_carte")) {
-            console.log(carte_choisie);
+            // Change style de la carte
             carte_choisie.style.transition = "backgroundcolor 0.5 ease-in-out,color 0.5 ease-in-out ";
             carte_choisie.style.backgroundColor = "white";
             carte_choisie.style.color = "white";
 
+            // Attendre avant de changer le style de la carte
             setTimeout(() => {
                 carte_choisie.style.backgroundColor = "black";
                 carte_choisie.style.color = "white";
+                // Remettre l'emoji de la carte
+                carte_choisie.textContent = emoji_id
                 setTimeout(trouverSiCartePaire,1000 * 2)
             }, 100 * 8)
         }
@@ -75,6 +85,7 @@ boxCarte.addEventListener("click", (e) => {
             setTimeout(() => {
                 carte_choisie.style.backgroundColor = "black";
                 carte_choisie.style.color = "white";
+                carte_choisie.textContent = e
                 changerEmoji(type_emoji)
             }, 100 * 8)
         }
@@ -97,16 +108,17 @@ boxCarte.addEventListener("click", (e) => {
 
     function trouverSiCartePaire(){
         liste_carte_paire.push(carte_choisie);
-        let i = 0
         const nombre_carte_paire = liste_carte_paire.filter(text => text.textContent ===carte_choisie.textContent).length
-        if (liste_carte_paire.length >= 2 ){
+        if (liste_carte_paire.length >= 2 * nombre_carte_valie ) {
             if ( nombre_carte_paire % 2 === 0){
                 alert("trouver")
+                nombre_carte_valie ++
             }
             else{
                 for (i = 0; i < liste_carte_paire.length;){
                     liste_carte_paire[i].textContent = "❓"
-                    liste_carte_paire.splice(i,1)
+                    liste_carte_paire.shift()
+                    nombre_carte_valie = 1
                 }
             }
         }
